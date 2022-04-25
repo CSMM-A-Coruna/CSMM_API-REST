14b0a59867ab51fe85ef83fd79c65d7c9b1e9f59
FlagshipDev <javimr2001@gmail.com>
2022-04-25 11:55:44 +0200

    Añadido cluster

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 // Arranque del servidor
 import app from './app'
+const cluster = require('cluster')
 
-
-// Iniciamos el servidor en el puerto definido
-app.listen(app.get('port'))
-console.log('Servidor iniciado en el puerto', app.get('port'))
+if(cluster.isMaster) {
+  let cpuCount =  require('os').cpus().length;
+  for(let i = 0; i < cpuCount; i++) {
+    cluster.fork()
+  }
+} else {
+  // Iniciamos el servidor en el puerto definido
+  app.listen(app.get('port'))
+  console.log(`Worker: ${cluster.worker.id}`, '- Servidor iniciado en el puerto', app.get('port'))
+}
