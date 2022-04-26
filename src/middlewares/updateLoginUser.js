ad89ec296ad0ed77c8cebb96bd32f2f36dabe9ec
FlagshipDev <javimr2001@gmail.com>
2022-04-26 09:45:13 +0200

    Arreglado error en sintaxis

--- src/middlewares/updateLoginUser.js
+++ src/middlewares/updateLoginUser.js
@@ -4,7 +4,7 @@ export const updateLoginUser = async (userId, userIp, device) => {
   try {
     const intAccesos = await executeQuery(`SELECT accesos FROM familias WHERE id = ${userId}`)
     const updateAccesos = await executeQuery(`UPDATE familias SET accesos = ${intAccesos[0].accesos+1} WHERE id = ${userId}`)
-    const updateIp = await executeQuery(`UPDATE familias SET ip = ${userIp} WHERE id = ${userId}`)
+    const updateIp = await executeQuery(`UPDATE familias SET ip = '${userIp}' WHERE id = ${userId}`)
     const updateDevice = await executeQuery(`UPDATE familias SET navegador = '${device}' WHERE id = ${userId}`)
     const date = new Date().toISOString()
     const updateUltimo = await executeQuery(`UPDATE familias SET ultimo = '${date}' WHERE id = ${userId}`)
